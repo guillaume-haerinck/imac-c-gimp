5fb2e38ac297fe84741d0e2567f9fbfec6f58acc
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2018-12-22 14:16:27 +0100

    Read ppm and its header

---
+++ src/image-loaders/ppm.h
@@ -0,0 +1,4 @@
+#ifndef MINIGIMP_PPM_H
+#define MINIGIMP_PPM_H
+
+#endif //MINIGIMP_PPM_H
--- src/main.c
+++ src/main.c
@@ -1,30 +1,53 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 // minigimp mon_image.ppm [-h] [-histo] [<code_lut>[_<param1>]*]* [-o image_sortie.ppm]
 
 int main(int argc, char *argv[]) {
-    printf("Input file is: %s \n", argv[1]);
-
-    /* Scan through args. */
-    for(int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "-histo") == 0) {
-            printf("Histogram asked \n");
-        } else if (strcmp(argv[i], "-o") == 0) {
-            printf("Output name is: %s\n", argv[i + 1]);
-        } else if (strcmp(argv[i], "ADDLUM") == 0) {
-            printf("Add luminosity filter power is: %s\n", argv[i + 1]);
-        } else if (strcmp(argv[i], "DIMLUM") == 0) {
-            printf("Dim luminosity filter power is: %s\n", argv[i + 1]);
-        } else if (strcmp(argv[i], "ADDCON") == 0) {
-            printf("Add constrast filter power is: %s\n", argv[i + 1]);
-        } else if (strcmp(argv[i], "DIMCON") == 0) {
-            printf("Dim constrast filter power is: %s\n", argv[i + 1]);
-        } else if (strcmp(argv[i], "INVERT") == 0) {
-            printf("Invert filter activated\n");
-        } else if (strcmp(argv[i], "SEPIA") == 0) {
-            printf("Sepia filter power is: %s\n", argv[i + 1]);
+    if (argc > 0) {
+        FILE* img;
+        int i, j, max;
+        char buf[255];
+        printf("Input file is: %s \n", argv[1]);
+        img = fopen(argv[1], "r");
+        if (img == NULL) {
+            perror("Failed to open file");
+            return EXIT_FAILURE;
         }
+        fscanf(img, "%s", buf);
+        printf("file format : %s\n", buf );
+        fscanf(img, "%s", buf);
+        printf("width : %s\n", buf );
+        fscanf(img, "%s", buf);
+        printf("heigth : %s\n", buf );
+        fscanf(img, "%s", buf);
+        printf("max colour : %s\n", buf );
+        fclose(img);
+
+        /* Scan through args. */
+        for(int i = 1; i < argc; i++) {
+            if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "-histo") == 0) {
+                printf("Histogram asked \n");
+            } else if (strcmp(argv[i], "-o") == 0) {
+                printf("Output name is: %s\n", argv[i + 1]);
+            } else if (strcmp(argv[i], "ADDLUM") == 0) {
+                printf("Add luminosity filter power is: %s\n", argv[i + 1]);
+            } else if (strcmp(argv[i], "DIMLUM") == 0) {
+                printf("Dim luminosity filter power is: %s\n", argv[i + 1]);
+            } else if (strcmp(argv[i], "ADDCON") == 0) {
+                printf("Add constrast filter power is: %s\n", argv[i + 1]);
+            } else if (strcmp(argv[i], "DIMCON") == 0) {
+                printf("Dim constrast filter power is: %s\n", argv[i + 1]);
+            } else if (strcmp(argv[i], "INVERT") == 0) {
+                printf("Invert filter activated\n");
+            } else if (strcmp(argv[i], "SEPIA") == 0) {
+                printf("Sepia filter power is: %s\n", argv[i + 1]);
+            }
+        }
+    } else {
+        printf("No input file provided ! \n");
+        return EXIT_FAILURE;
     }
-    return 0;
+    return EXIT_SUCCESS;
 }
