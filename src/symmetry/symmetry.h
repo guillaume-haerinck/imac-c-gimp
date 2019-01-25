101c67eb781bacfe13e7d05f9fedbc4edf236416
Nicolas LIENART <nicolas@lienart.net>
2019-01-25 22:05:57 +0100

    Symmetry.h commenting

--- src/symmetry/symmetry.h
+++ src/symmetry/symmetry.h
@@ -3,8 +3,18 @@
 
 #include "core/imac-img.h"
 
+/**
+ * @brief Flip image from a vertical axis 
+ *
+ * @param[in] image
+ */
 void symmetry_flipV(ImacImg* img);
 
+/**
+ * @brief Flip image from a horizontal axis 
+ *
+ * @param[in] image
+ */
 void symmetry_flipH(ImacImg* img);
 
 #endif // MINIGIMP_SYMMETRY_H
